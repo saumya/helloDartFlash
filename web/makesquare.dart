0aeaeb8631b05c2252b431c8e9be5e67adcca451
saumya <saumya>
2013-02-20 15:50:22 +0530

    inital setup

--- web/makesquare.dart
+++ web/makesquare.dart
@@ -8,6 +8,21 @@ void main() {
     ..text = "Click me!"
     ..onClick.listen(reverseText);
   */
+  initialiseApplication();
+  
+}
+/*
+void reverseText(MouseEvent event) {
+  var text = html.query("#sample_text_id").text;
+  var buffer = new StringBuffer();
+  for (int i = text.length - 1; i >= 0; i--) {
+    buffer.add(text[i]);
+  }
+  html.query("#sample_text_id").text = buffer.toString();
+}
+*/
+void initialiseApplication()
+{
   var stage= new Stage('stage',html.query('#flash_container_id'));
   var renderLoop = new RenderLoop();
   renderLoop.addStage(stage);
@@ -21,22 +36,29 @@ void main() {
   // Create a Bitmap with the nice looking BitmapData
 
   Bitmap bitmap = new Bitmap(bitmapData);
-  bitmap.x = 100;
-  bitmap.y = 100;
-  bitmap.rotation = PI / 8.0;
+  bitmap.x = 10;
+  bitmap.y = 10;
+  //bitmap.rotation = PI / 8.0;
+  //bitmap.addEventListener(MouseEvent.CLICK, eventListener, useCapture)
+  Sprite s=new Sprite();
+  s.addChild(bitmap);
+  s.addEventListener(MouseEvent.CLICK, onUserClick);
+  //html.window.console.debug("debug message");
+  logThis('Debug');
 
   // Add the Bitmap to the Stage
+  //stage.addChild(bitmap);
+  stage.addChild(s);
+}
 
-  stage.addChild(bitmap);
-  
+void onUserClick(MouseEvent e)
+{
+  //alert.show('Hello');
+  //console.log(e);
+  logThis(e);
 }
-/*
-void reverseText(MouseEvent event) {
-  var text = html.query("#sample_text_id").text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.add(text[i]);
-  }
-  html.query("#sample_text_id").text = buffer.toString();
+
+void logThis(Object message)
+{
+  html.window.console.debug(message);
 }
-*/
